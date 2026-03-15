7823505994b7553b8c91d5f3fece3ce5a3ed138f
DevMonkey3 <112334514+DevMonkey3@users.noreply.github.com>
2026-03-15 19:42:37 +0600

    Update footer.tsx

--- components/footer/footer.tsx
+++ components/footer/footer.tsx
@@ -259,9 +259,29 @@ const Footer: React.FC = () => {
                   </Link>
                 ))}
               </div>
-              <p className="footer-bottom-text">
-                Copyright © {CURRENT_YEAR} Jbbra. All Rights Reserved.
-              </p>
+              <div className="flex items-center gap-4">
+                {/* Facebook Social Link */}
+                <a
+                  href="https://www.facebook.com/JBBRAbd/"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="text-blue-200 hover:text-white transition-colors"
+                  aria-label="Follow us on Facebook"
+                >
+                  <svg
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                    fill="currentColor"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path d="M24 12.073c0-6.627-5.373-12-12-12s-12 5.373-12 12c0 5.99 4.388 10.954 10.125 11.854v-8.385H7.078v-3.47h3.047V9.43c0-3.007 1.792-4.669 4.533-4.669 1.312 0 2.686.235 2.686.235v2.953H15.83c-1.491 0-1.956.925-1.956 1.874v2.25h3.328l-.532 3.47h-2.796v8.385C19.612 23.027 24 18.062 24 12.073z"/>
+                  </svg>
+                </a>
+                <p className="footer-bottom-text">
+                  Copyright © {CURRENT_YEAR} Jbbra. All Rights Reserved.
+                </p>
+              </div>
             </div>
           </div>
         </div>
